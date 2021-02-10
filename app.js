c818e76a65f560717a8edcbced7525e18e6f6ae5
Halleys-Comet <jhalley230@gmail.com>
2021-02-09 20:03:42 -0600

    add inquirer

--- app.js
+++ app.js
@@ -1,28 +1,23 @@
-const fs = require ('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require ('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-const [name, github] = profileDataArgs;
-
+// cosnt pageHTML = generatePage(name,github);
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
 
-    console.log('Portfolio complete! Check out index.html to see the output!')
-});
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
 
-// const printProfileData = profileDataArr => {
-//     // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// });
 
-//   console.log('================');
-
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
+inquirer 
+    .prompt([
+        {
+            type: 'input', 
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
